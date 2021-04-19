3c5e423b5e92898a474876db5e62de937c890c2c
cquest <cquest@openstreetmap.fr>
2021-04-19 14:42:00 +0200

    factoring cur.execute + timing

--- folksonomy/api.py
+++ folksonomy/api.py
@@ -49,6 +49,12 @@ async def hello():
     return {"message": "Hello folksonomy World"}
 
 
+async def db_exec(response, query):
+    t = time.time()
+    cur.execute(query)
+    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
+
+
 
 # define route for authentication
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
@@ -111,15 +117,14 @@ INSERT INTO auth (user_id, token, last_use) VALUES ('%s','%s',current_timestamp 
         status_code=500, detail="Server error")
 
 
-
 @app.get("/products/", response_model=List[ProductStats])
     # product list by owner + number of keys,, editors and last_edit
 async def product_list(response: Response, owner='', user: User = Depends(get_current_user)):
     if owner != '':
         if owner != user:
             return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user, owner)}})
-    t = time.time()
-    query = cur.mogrify("""
+
+    await db_exec(response, cur.mogrify("""
 SELECT json_agg(j.j)::json FROM(
     SELECT json_build_object(
         'product',product,
@@ -130,53 +135,50 @@ SELECT json_agg(j.j)::json FROM(
     FROM folksonomy 
     WHERE owner=%s
     GROUP BY product) as j;
-""", (owner,))
-    cur.execute(query)
+""", (owner,)))
     out = cur.fetchone()
-    response.headers['x-pg-timing'] = str(round(time.time()-t,4)*1000)+"ms"
     return out[0]
 
 
-# product list by owner + number of keys,, editors and last_edit
 @app.get("/product/{product}", response_model=List[ProductTag])
-async def product_detail(response: Response, product: str, owner='', user: User = Depends(get_current_user)):
+async def product_tags_list(response: Response, product: str, owner='', user: User = Depends(get_current_user)):
     if owner != '':
         if owner != user:
             return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user, owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    await db_exec(response, cur.mogrify("""
 SELECT json_agg(j)::json FROM(
     SELECT *
     FROM folksonomy 
     WHERE product = %s AND owner = %s
     ORDER BY k
     ) as j;
-""", (product, owner))
-    cur.execute(query)
+""", (product, owner)))
     out = cur.fetchone()
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
-    return out[0]
+    if out:
+        return out[0]
+    else:
+        return
 
 
 @app.get("/product/{product}/{k}", response_model=ProductTag)
-async def product_tag_list_versions(response: Response, product: str, k: str, owner='', user: User = Depends(get_current_user)):
+async def product_tag(response: Response, product: str, k: str, owner='', user: User = Depends(get_current_user)):
     if owner != '':
         if owner != user:
             return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user, owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    await db_exec(response, cur.mogrify("""
 SELECT row_to_json(j) FROM(
     SELECT *
     FROM folksonomy
     WHERE product = %s AND owner = %s AND k = %s
     ) as j;
-""", (product, owner, k))
-    cur.execute(query)
+""", (product, owner, k)))
     out = cur.fetchone()
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
-    return out[0]
+    if out:
+        return out[0]
+    else:
+        return
 
 
 @app.get("/product/{product}/{k}/versions", response_model=List[ProductTag])
@@ -186,19 +188,19 @@ async def product_tag_list_versions(response: Response, product: str, k: str, ow
         if owner != user:
             return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user, owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    await db_exec(response, cur.mogrify("""
 SELECT json_agg(j)::json FROM(
     SELECT *
     FROM folksonomy_versions
     WHERE product = %s AND owner = %s AND k = %s
     ORDER BY version DESC
     ) as j;
-""", (product, owner, k))
-    cur.execute(query)
+""", (product, owner, k)))
     out = cur.fetchone()
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
-    return out[0]
+    if out:
+        return out[0]
+    else:
+        return
 
 
 @app.get("/product/{product}/{k}/version/{version}", response_model=ProductTag)
@@ -207,17 +209,14 @@ async def product_tag_version(response: Response, product: str, k: str, version:
         if owner != user:
             return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user, owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    await db_exec(response, cur.mogrify("""
 SELECT row_to_json(j) FROM(
     SELECT *
     FROM folksonomy_versions
     WHERE product = %s AND owner = %s AND k = %s and version = %s
     ) as j;
-""", (product, owner, k, version))
-    cur.execute(query)
+""", (product, owner, k, version)))
     out = cur.fetchone()
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
     return out[0]
 
 
@@ -229,8 +228,8 @@ async def product_tag_add(  response: Response,
     if product_tag.owner not in ('', user["user_id"]):
         return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user["user_id"], product_tag.owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    try:
+        await db_exec(response, cur.mogrify("""
 INSERT INTO folksonomy (product,k,v,owner,version,editor,comment)
     VALUES ('%s','%s','%s','%s', %s,'%s','%s')
     """ % ( product_tag.product,
@@ -240,16 +239,14 @@ INSERT INTO folksonomy (product,k,v,owner,version,editor,comment)
             product_tag.version,
             user["user_id"],
             product_tag.comment
-             ))
-    try:
-        cur.execute(query)
+             )))
     except psycopg2.Error as e:
         error_msg = re.sub(r'.*@@ (.*) @@\n.*$', r'\1', e.pgerror)[:-1]
         return JSONResponse(status_code=422, content={"detail": {"msg": error_msg}})
+
     db.commit()
     if cur.rowcount == 1:
         return "ok"
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
     return
 
 
@@ -261,24 +258,22 @@ async def product_tag_update(   response: Response,
     if product_tag.owner not in ('', user["user_id"]):
         return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user["user_id"], product_tag.owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    try:
+        await db_exec(response, cur.mogrify("""
 UPDATE folksonomy SET 
     v = '%s',
     version = %s,
     editor = '%s',
     comment = '%s'
     WHERE product = '%s' AND owner = '%s' AND k = '%s'
-    """ % (product_tag.v, product_tag.version, user["user_id"], product_tag.comment, product_tag.product, product_tag.owner, product_tag.k))
-    try:
-        cur.execute(query)
+    """ % (product_tag.v, product_tag.version, user["user_id"], product_tag.comment, product_tag.product, product_tag.owner, product_tag.k)))
     except psycopg2.Error as e:
         error_msg = re.sub(r'.*@@ (.*) @@\n.*$',r'\1',e.pgerror)[:-1]
         return JSONResponse(status_code=422, content={"detail": {"msg": error_msg}})
+
     db.commit()
     if cur.rowcount == 1:
         return "ok"
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
     return
 
 
@@ -293,12 +288,10 @@ async def product_tag_delete(   response: Response,
     if product_tag.owner not in ('', user["user_id"]):
         return JSONResponse(status_code=422, content={"detail": {"msg": "owner should be '%s' or '' for public, was '%s'" % (user["user_id"], product_tag.owner)}})
 
-    t = time.time()
-    query = cur.mogrify("""
+    await db_exec(response, cur.mogrify("""
 DELETE FROM folksonomy WHERE product = '%s' AND owner = '%s' AND k = '%s' AND version = %s
-    """ % (product, owner, k, version))
-    cur.execute(query)
+    """ % (product, owner, k, version)))
+    db.commit()
     if cur.rowcount == 1:
         return "ok"
-    response.headers['x-pg-timing'] = str(round(time.time()-t, 4)*1000)+"ms"
     return
