f5a3e540da99a5eae58e1ee0bfdbc1e32154a4e6
Charles Nepote <charles@nepote.org>
2025-02-24 10:15:06 +0100

    Default value for POSTGRES_HOST

--- local_settings_example.py
+++ local_settings_example.py
@@ -5,8 +5,8 @@ OFF_TEST_PASSWORD = 'mypassword'
 # Postgres
 POSTGRES_USER = '' # Leave empty if no user exists for database
 POSTGRES_PASSWORD = '' # Leave empty if no password exists for user
-POSTGRES_HOST = '' # Change if necessary
-POSTGRES_DATABASE = 'folksonomy/'
+POSTGRES_HOST = '127.0.0.1' # Change if necessary
+POSTGRES_DATABASE = 'folksonomy'
 
 # for dev using a local product opener instance you can use this
 # base domain has to be the same (see INSTALL.md)
