d1ba3c2311c78b051106773bf778305f855a62f6
Alex Garel <alex@garel.org>
2025-08-14 17:56:55 +0200

    fix: allow_methods must be explicit with allow_credentials

--- folksonomy/api.py
+++ folksonomy/api.py
@@ -104,13 +104,9 @@ app.add_middleware(
     # So, for everything to work correctly, it's better to specify explicitly the allowed origins."
     # => Workarround: use allow_origin_regex
     # Source: https://github.com/tiangolo/fastapi/issues/133#issuecomment-646985050
-    allow_origins=[
-        "http://localhost:8000",
-        "http://world.openfoodfacts.localhost",
-    ],
     allow_origin_regex="https?://.*",
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
     expose_headers=["*"],
 )
